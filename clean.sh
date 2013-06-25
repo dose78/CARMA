d7de82909c522b0b8fa58c014bd0255ff4d9c1f8
David Eliahu <davideliahu@gmail.com>
2013-06-25 12:15:17 -0700

    merge job-carma logic with run.sh

---
+++ clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+
+rm -f *.csv
+rm -f data_gatherer
+rm -f data_gatherer-*
+rm -f nohup.out
+rm -f *.o*
+rm -f *.tmp
+clear
--- run.sh
+++ run.sh
@@ -1,120 +1,108 @@
 #!/bin/bash
 
-MIN_M=64 # All dimensions should be multiples of 64
+MIN_M=1024 # All dimensions should be multiples of 64
 MAX_M=1024
-MIN_K=64
-MAX_K=1024
-MIN_N=64
+MIN_K=1024
+MAX_K=2048
+MIN_N=512
 MAX_N=1024
-MIN_THREADS=32
+
+MIN_THREADS=16
 MAX_THREADS=32 # Sweep on threads is exponential (mult factor of 2)
-CARMA_DEPTH_MIN=5 # Recommended to be at least log2(# cores)
+CARMA_DEPTH_MIN=4 # Recommended to be at least log2(# cores)
 CARMA_DEPTH_MAX=5 # Sweep on depth is linear (increment of 1)
+
+MODE=sweep # Must be "sweep" or "random"
 SWEEP_PATTERN=exp # Must be "exp" or "linear"
-SWEEP_SUM_INCREMENT=64 # Increment for linear sweeps
-SWEEP_MULT_FACTOR=2 # Multiplication factor for exponential sweeps
+SWEEP_CONSTANT=2 # Multiplication factor for exponential sweeps or increment for linear sweeps
+
+ALGS=both # Must be "carma" "mkl" or "both"
+PRECISION=single # Must be "single" or "double"
+
+ITERATIONS=2
+REPETITIONS=3
+
 RESERVE=no # For Emerald or Sandy only, use "yes" or "no"
 
-## YOU SHOLDN'T HAVE TO EDIT BELOW THIS LINE ##
+## ONLY EDIT BELOW THIS LINE TO ADD MACHINES ##
 
-runSweepLinear () {
-  for (( m=$MIN_M; m<=$MAX_M; m+=$SWEEP_SUM_INCREMENT )); do
-    for (( k=$MIN_K; k<=$MAX_K; k+=$SWEEP_SUM_INCREMENT )); do
-      for (( n=$MIN_N; n<=$MAX_N; n+=$SWEEP_SUM_INCREMENT )); do
-        ./data_gatherer-$base_alg $alg $m $k $n $threads $carma_depth $repetitions
-      done
-    done
-  done
-}
-
-runSweepExp () {
-  for (( m=$MIN_M; m<=$MAX_M; m*=$SWEEP_MULT_FACTOR )); do
-    for (( k=$MIN_K; k<=$MAX_K; k*=$SWEEP_MULT_FACTOR )); do
-      for (( n=$MIN_N; n<=$MAX_N; n*=$SWEEP_MULT_FACTOR )); do
-        ./data_gatherer-$base_alg $alg $m $k $n $threads $carma_depth $repetitions
-      done
-    done
-  done
-}
-
-runRandom () {
-  if [ $alg == ${algs[0]} ]; then # use same dimensions if multiple algs
-    m=$(((($RANDOM % (($MAX_M - $MIN_M) / 32 + 1)) * 32) + $MIN_M))
-    k=$(((($RANDOM % (($MAX_K - $MIN_K) / 32 + 1)) * 32) + $MIN_K))
-    n=$(((($RANDOM % (($MAX_N - $MIN_N) / 32 + 1)) * 32) + $MIN_N))
+myInit () {
+  if [ $NERSC_HOST ] && [ $NERSC_HOST = "hopper" ]; then
+    machine="hopper"
+  elif [ $HOST ] && [ $HOST = "emerald" ]; then
+    machine="emerald"
+  elif [ $HOST ] && [ $HOST = "sandy" ]; then
+    machine="sandy"
   fi
-  ./data_gatherer-$base_alg $alg $m $k $n $threads $carma_depth $repetitions
+
+  if [ $machine = "hopper" ]; then
+    run_command="ccmrun mpirun -np 1"
+    flags="-mkl"
+    compiler="mpicc"
+  elif [ $machine = "emerald" ] || [ $machine = "sandy" ]; then
+    run_command=""
+    flags="-mkl -O3 -ipo -xHOST -no-prec-div -fno-strict-aliasing -fno-omit-frame-pointer"
+    compiler="icc"
+    if [ $RESERVE = "yes" ]; then
+      /reserve/reserve.me
+    fi
+  else # General case
+    run_command=""
+    flags="-mkl -O3 -ipo -xHOST -no-prec-div -fno-strict-aliasing -fno-omit-frame-pointer"
+    compiler="icc"
+  fi
 }
 
 myExit () {
-  echo -e "\e[01;32mThis trial took" $SECONDS "seconds\e[0m"
-  rm -rf data_gatherer-*
-
-  if [ `echo $RESERVE | tr [:upper:] [:lower:]` = "yes" ]; then
-    /reserve/unreserve.me
+  if [ $machine = emerald ] || [ $machine = sandy ]; then
+    if [ $RESERVE = "yes" ]; then
+      /reserve/unreserve.me
+    fi
   fi
-  exit
 }
 
-if [ `echo $RESERVE | tr [:upper:] [:lower:]` = "yes" ]; then
-  /reserve/reserve.me
-fi
+## YOU SHOLDN'T HAVE TO EDIT BELOW THIS LINE ##
 
-if [ $# -ne 5 ]; then
-  echo -e "\e[0;31mUSAGE: ./run.sh carma|mkl|both single|double sweep|random <#iterations> <#repetitions>\e[0m"
-  myExit
-fi
+myInit
+echo -e "\e[01;34mMachine: $machine\e[0m"
 
-cd "$( dirname "$0" )"
 rm -f data.csv
 echo "algorithm,m,k,n,carma_depth,threads,gflop/s" > data.csv
 
 algs=()
-in_algs=$1
-if [ `echo $in_algs | tr [:upper:] [:lower:]` = "carma" ]; then
+if [ $ALGS = "carma" ]; then
   for (( depth=$CARMA_DEPTH_MIN; depth<=$CARMA_DEPTH_MAX; depth+=1 )); do
       algs+=(carma_$depth)
   done
-elif [ `echo $in_algs | tr [:upper:] [:lower:]` = "mkl" ]; then
+elif [ $ALGS = "mkl" ]; then
   algs=( mkl )
-elif [ `echo $in_algs | tr [:upper:] [:lower:]` = "both" ]; then
+elif [ $ALGS = "both" ]; then
   for (( depth=$CARMA_DEPTH_MIN; depth<=$CARMA_DEPTH_MAX; depth+=1 )); do
       algs+=(carma_$depth)
   done
   algs+=(mkl)
-else
-  echo -e "\e[0;31mERROR: You must specify either \"carma\", \"mkl\", or \"both\" algorithms\e[0m"
-  myExit
 fi
 
-precision=$2
-echo -e "\e[01;34mcompiling algorithms...\e[0m"
-FLAGS="-mkl -O3 -ipo -xHOST -no-prec-div -fno-strict-aliasing -fno-omit-frame-pointer"
-if [ `echo $precision | tr [:upper:] [:lower:]` = "single" ]; then
-  icc $FLAGS -o data_gatherer-carma data_gatherer_single.c carma_single.c
-  icc $FLAGS -o data_gatherer-mkl data_gatherer_single.c mkl_single.c
-elif [ `echo $precision | tr [:upper:] [:lower:]` = "double" ]; then
-  icc $FLAGS -o data_gatherer-carma data_gatherer_double.c carma_double.c
-  icc $FLAGS -o data_gatherer-mkl data_gatherer_double.c mkl_double.c
-else
-  echo -e "\e[0;31mERROR: You must specify either \"single\" or \"double\" precision\e[0m"
-  myExit
+echo -e "\e[01;34mCompiling algorithms...\e[0m"
+if [ $PRECISION = "single" ]; then
+  $compiler $flags -o data_gatherer-carma data_gatherer_single.c carma_single.c
+  $compiler $flags -o data_gatherer-mkl data_gatherer_single.c mkl_single.c
+elif [ $PRECISION = "double" ]; then
+  $compiler $flags -o data_gatherer-carma data_gatherer_double.c carma_double.c
+  $compiler $flags -o data_gatherer-mkl data_gatherer_double.c mkl_double.c
 fi
 
-iterations=$4
-repetitions=$5
-echo -e "\e[01;34mwill run $iterations iterations, $repetitions repetitions per iteration\e[0m"
+echo -e "\e[01;34mWill run $ITERATIONS iterations, $REPETITIONS repetitions per iteration\e[0m"
 
 export MKL_DYNAMIC=FALSE
 
-mode=$3
-for (( i=1; i<=$iterations; i++ )); do
-  echo -e "\e[01;36mrunning iteration $i\e[0m"
+for (( i=1; i<=$ITERATIONS; i++ )); do
+  echo -e "\e[01;36mRunning iteration $i\e[0m"
   for alg in "${algs[@]}"; do
-    echo -e "\e[0;32mrunning $alg...\e[0m"
+    echo -e "\e[0;32mRunning $alg...\e[0m"
     base_alg=$(cut -d "_" -f 1 <<< "$alg")
     for (( threads=$MIN_THREADS; threads<=$MAX_THREADS; threads*=2 )); do
-      if [ `echo $alg | tr [:upper:] [:lower:]` = "mkl" ]; then
+      if [ $alg = "mkl" ]; then
         export MKL_NUM_THREADS=$threads
         carma_depth=-1
       else
@@ -122,33 +110,28 @@ for (( i=1; i<=$iterations; i++ )); do
         export CILK_NWORKERS=$threads
         carma_depth=$(cut -d "_" -f 2 <<< "$alg")
       fi
-      if [ `echo $mode | tr [:upper:] [:lower:]` = "sweep" ]; then
-        if [ `echo $SWEEP_PATTERN | tr [:upper:] [:lower:]` = "linear" ]; then
-          runSweepLinear
-        elif [ `echo $SWEEP_PATTERN | tr [:upper:] [:lower:]` = "exp" ]; then
-          runSweepExp
-        else
-          echo -e "\e[0;31mERROR: You must specify either \"linear\" or \"exp\" sweep pattern in run.sh\e[0m"
-          myExit
+      if [ $MODE = "sweep" ]; then
+        echo $run_command ./data_gatherer-$base_alg $alg $MIN_M $MIN_K $MIN_N $MAX_M $MAX_K $MAX_N $threads $carma_depth $REPETITIONS $SWEEP_PATTERN $SWEEP_CONSTANT
+      elif [ $MODE = "random" ]; then
+        if [ $alg == ${algs[0]} ]; then # else use same dimensions if multiple algs
+          m=$(((($RANDOM % (($MAX_M - $MIN_M) / 32 + 1)) * 32) + $MIN_M))
+          k=$(((($RANDOM % (($MAX_K - $MIN_K) / 32 + 1)) * 32) + $MIN_K))
+          n=$(((($RANDOM % (($MAX_N - $MIN_N) / 32 + 1)) * 32) + $MIN_N))
         fi
-      elif [ `echo $mode | tr [:upper:] [:lower:]` = "random" ]; then
-        runRandom
-      else
-        echo -e "\e[0;31mERROR: You must specify either \"random\" or \"sweep\"\e[0m"
-        myExit
+        echo $run_command ./data_gatherer-$base_alg $alg $m $k $n $m $k $n $threads $carma_depth $REPETITIONS $SWEEP_PATTERN $SWEEP_CONSTANT
       fi
     done
   done
 done
 
 echo -e "\e[0;36mcollating data...\e[0m"
-if [ `echo $mode | tr [:upper:] [:lower:]` = "sweep" ]; then
-  python collator.py $((iterations * repetitions))
-elif [ `echo $mode | tr [:upper:] [:lower:]` = "random" ]; then
-  python collator.py $repetitions
-else
-  echo -e "\e[0;31mERROR: You must specify either \"random\" or \"sweep\"\e[0m"
-  myExit
+if [ $MODE  = "sweep" ]; then
+  python collator.py $((ITERATIONS * REPETITIONS))
+elif [ $MODE = "random" ]; then
+  python collator.py $REPETITIONS
 fi
+
+echo -e "\e[01;32mThis trial took" $SECONDS "seconds\e[0m"
+rm -rf data_gatherer-*
 
 myExit
