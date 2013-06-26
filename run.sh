9f8a467a6dcef70b3fac7a6e75eb73b1675dff8a
David Eliahu <davideliahu@gmail.com>
2013-06-26 12:42:08 -0700

    allow for multiple batch scripts to be queued in parallel

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ MAX_K=2048
 MIN_N=1024
 MAX_N=1024
 
-MIN_THREADS=16
-MAX_THREADS=32 # Sweep on threads is exponential (mult factor of 2)
+MIN_THREADS=12
+MAX_THREADS=24 # Sweep on threads is exponential (mult factor of 2)
 CARMA_DEPTH_MIN=4 # Recommended to be at least log2(# cores)
-CARMA_DEPTH_MAX=5 # Sweep on depth is linear (increment of 1)
+CARMA_DEPTH_MAX=4 # Sweep on depth is linear (increment of 1)
 
 MODE=sweep # Must be "sweep" or "random"
 SWEEP_PATTERN=exp # Must be "exp" or "linear"
@@ -26,116 +26,10 @@ RESERVE=no # For Emerald or Sandy only, use "yes" or "no"
 
 ## ONLY EDIT BELOW THIS LINE TO ADD MACHINES ##
 
-myInit () {
-  if [ $NERSC_HOST ] && [ $NERSC_HOST = "hopper" ]; then
-    machine="hopper"
-  elif [ $HOST ] && [ $HOST = "emerald" ]; then
-    machine="emerald"
-  elif [ $HOST ] && [ $HOST = "sandy" ]; then
-    machine="sandy"
-  fi
+OUTPUT=${1:-"data.csv"}
 
-  if [ $machine = "hopper" ]; then
-    run_command="ccmrun mpirun -np 1"
-    flags="-mkl"
-    compiler="mpicc"
-  elif [ $machine = "emerald" ] || [ $machine = "sandy" ]; then
-    run_command=""
-    flags="-mkl -O3 -ipo -xHOST -no-prec-div -fno-strict-aliasing -fno-omit-frame-pointer"
-    compiler="icc"
-    source /opt/intel/bin/iccvars.sh intel64
-    if [ $RESERVE = "yes" ]; then
-      /reserve/reserve.me
-    fi
-  else # General case
-    run_command=""
-    flags="-mkl -O3 -ipo -xHOST -no-prec-div -fno-strict-aliasing -fno-omit-frame-pointer"
-    compiler="icc"
-  fi
-}
-
-myExit () {
-  if [ $machine = emerald ] || [ $machine = sandy ]; then
-    if [ $RESERVE = "yes" ]; then
-      /reserve/unreserve.me
-    fi
-  fi
-}
-
-## YOU SHOLDN'T HAVE TO EDIT BELOW THIS LINE ##
-
-myInit
-echo -e "\e[01;34mMachine: $machine\e[0m"
-
-output=${1:-"data.csv"}
-echo -e "\e[01;34mOutput file: $output\e[0m"
-
-rm -f $output
-echo "algorithm,m,k,n,carma_depth,threads,gflop/s" > $output
-
-algs=()
-if [ $ALGS = "carma" ]; then
-  for (( depth=$CARMA_DEPTH_MIN; depth<=$CARMA_DEPTH_MAX; depth+=1 )); do
-      algs+=(carma_$depth)
-  done
-elif [ $ALGS = "mkl" ]; then
-  algs=( mkl )
-elif [ $ALGS = "both" ]; then
-  for (( depth=$CARMA_DEPTH_MIN; depth<=$CARMA_DEPTH_MAX; depth+=1 )); do
-      algs+=(carma_$depth)
-  done
-  algs+=(mkl)
+if [ $NERSC_HOST ] && [ $NERSC_HOST = "hopper" ]; then
+  qsub job-hopper.pbs -v MIN_M=$MIN_M,MAX_M=$MAX_M,MIN_K=$MIN_K,MAX_K=$MAX_K,MIN_N=$MIN_N,MAX_N=$MAX_N,MIN_THREADS=$MIN_THREADS,MAX_THREADS=$MAX_THREADS,CARMA_DEPTH_MIN=$CARMA_DEPTH_MIN,CARMA_DEPTH_MAX=$CARMA_DEPTH_MAX,MODE=$MODE,SWEEP_PATTERN=$SWEEP_PATTERN,SWEEP_CONSTANT=$SWEEP_CONSTANT,ALGS=$ALGS,PRECISION=$PRECISION,ITERATIONS=$ITERATIONS,REPETITIONS=$REPETITIONS,RESERVE=$RESERVE,OUTPUT=$OUTPUT
+else
+  ./sweep.sh $MIN_M $MAX_M $MIN_K $MAX_K $MIN_N $MAX_N $MIN_THREADS $MAX_THREADS $CARMA_DEPTH_MIN $CARMA_DEPTH_MAX $MODE $SWEEP_PATTERN $SWEEP_CONSTANT $ALGS $PRECISION $ITERATIONS $REPETITIONS $RESERVE $OUTPUT
 fi
-
-echo -e "\e[01;34mCompiling algorithms...\e[0m"
-if [ $PRECISION = "single" ]; then
-  $compiler $flags -o data_gatherer-carma data_gatherer_single.c carma_single.c
-  $compiler $flags -o data_gatherer-mkl data_gatherer_single.c mkl_single.c
-elif [ $PRECISION = "double" ]; then
-  $compiler $flags -o data_gatherer-carma data_gatherer_double.c carma_double.c
-  $compiler $flags -o data_gatherer-mkl data_gatherer_double.c mkl_double.c
-fi
-
-echo -e "\e[01;34mWill run $ITERATIONS iterations, $REPETITIONS repetitions per iteration\e[0m"
-
-export MKL_DYNAMIC=FALSE
-
-for (( i=1; i<=$ITERATIONS; i++ )); do
-  echo -e "\e[01;36mRunning iteration $i\e[0m"
-  for alg in "${algs[@]}"; do
-    echo -e "\e[0;32mRunning $alg...\e[0m"
-    base_alg=$(cut -d "_" -f 1 <<< "$alg")
-    for (( threads=$MIN_THREADS; threads<=$MAX_THREADS; threads*=2 )); do
-      if [ $alg = "mkl" ]; then
-        export MKL_NUM_THREADS=$threads
-        carma_depth=-1
-      else
-        export MKL_NUM_THREADS=1
-        export CILK_NWORKERS=$threads
-        carma_depth=$(cut -d "_" -f 2 <<< "$alg")
-      fi
-      if [ $MODE = "sweep" ]; then
-        $run_command ./data_gatherer-$base_alg $alg $MIN_M $MIN_K $MIN_N $MAX_M $MAX_K $MAX_N $threads $carma_depth $REPETITIONS $SWEEP_PATTERN $SWEEP_CONSTANT $output
-      elif [ $MODE = "random" ]; then
-        if [ $alg == ${algs[0]} ]; then # else use same dimensions if multiple algs
-          m=$(((($RANDOM % (($MAX_M - $MIN_M) / 32 + 1)) * 32) + $MIN_M))
-          k=$(((($RANDOM % (($MAX_K - $MIN_K) / 32 + 1)) * 32) + $MIN_K))
-          n=$(((($RANDOM % (($MAX_N - $MIN_N) / 32 + 1)) * 32) + $MIN_N))
-        fi
-        $run_command ./data_gatherer-$base_alg $alg $m $k $n $m $k $n $threads $carma_depth $REPETITIONS $SWEEP_PATTERN $SWEEP_CONSTANT $output
-      fi
-    done
-  done
-done
-
-echo -e "\e[0;36mcollating data...\e[0m"
-if [ $MODE  = "sweep" ]; then
-  python collator.py $((ITERATIONS * REPETITIONS)) $output
-elif [ $MODE = "random" ]; then
-  python collator.py $REPETITIONS $output
-fi
-
-echo -e "\e[01;32mThis trial took" $SECONDS "seconds\e[0m"
-rm -rf data_gatherer-*
-
-myExit
---
+++ sweep.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+## ONLY EDIT BELOW THIS LINE TO ADD PARAMETERS ##
+
+MIN_M=${1}
+MAX_M=${2}
+MIN_K=${3}
+MAX_K=${4}
+MIN_N=${5}
+MAX_N=${6}
+
+MIN_THREADS=${7}
+MAX_THREADS=${8}
+CARMA_DEPTH_MIN=${9}
+CARMA_DEPTH_MAX=${10}
+
+MODE=${11}
+SWEEP_PATTERN=${12}
+SWEEP_CONSTANT=${13}
+
+ALGS=${14}
+PRECISION=${15}
+
+ITERATIONS=${16}
+REPETITIONS=${17}
+
+RESERVE=${18}
+
+OUTPUT=${19}
+
+## ONLY EDIT BELOW THIS LINE TO ADD MACHINES ##
+
+myInit () {
+  if [ $NERSC_HOST ] && [ $NERSC_HOST = "hopper" ]; then
+    machine="hopper"
+  elif [ $HOST ] && [ $HOST = "emerald" ]; then
+    machine="emerald"
+  elif [ $HOST ] && [ $HOST = "sandy" ]; then
+    machine="sandy"
+  fi
+
+  if [ $machine = "hopper" ]; then
+    run_command="ccmrun mpirun -np 1"
+    flags="-mkl"
+    compiler="mpicc"
+  elif [ $machine = "emerald" ] || [ $machine = "sandy" ]; then
+    run_command=""
+    flags="-mkl -O3 -ipo -xHOST -no-prec-div -fno-strict-aliasing -fno-omit-frame-pointer"
+    compiler="icc"
+    source /opt/intel/bin/iccvars.sh intel64
+    if [ $RESERVE = "yes" ]; then
+      /reserve/reserve.me
+    fi
+  else # General case
+    run_command=""
+    flags="-mkl -O3 -ipo -xHOST -no-prec-div -fno-strict-aliasing -fno-omit-frame-pointer"
+    compiler="icc"
+  fi
+}
+
+myExit () {
+  if [ $machine = emerald ] || [ $machine = sandy ]; then
+    if [ $RESERVE = "yes" ]; then
+      /reserve/unreserve.me
+    fi
+  fi
+}
+
+## YOU SHOLDN'T HAVE TO EDIT BELOW THIS LINE ##
+
+myInit
+echo -e "\e[01;34mMachine: $machine\e[0m"
+echo -e "\e[01;34mOutput file: $OUTPUT\e[0m"
+
+rm -f $OUTPUT
+echo "algorithm,m,k,n,carma_depth,threads,gflop/s" > $OUTPUT
+
+algs=()
+if [ $ALGS = "carma" ]; then
+  for (( depth=$CARMA_DEPTH_MIN; depth<=$CARMA_DEPTH_MAX; depth+=1 )); do
+      algs+=(carma_$depth)
+  done
+elif [ $ALGS = "mkl" ]; then
+  algs=( mkl )
+elif [ $ALGS = "both" ]; then
+  for (( depth=$CARMA_DEPTH_MIN; depth<=$CARMA_DEPTH_MAX; depth+=1 )); do
+      algs+=(carma_$depth)
+  done
+  algs+=(mkl)
+fi
+
+echo -e "\e[01;34mCompiling algorithms...\e[0m"
+if [ $PRECISION = "single" ]; then
+  $compiler $flags -o data_gatherer-carma data_gatherer_single.c carma_single.c
+  $compiler $flags -o data_gatherer-mkl data_gatherer_single.c mkl_single.c
+elif [ $PRECISION = "double" ]; then
+  $compiler $flags -o data_gatherer-carma data_gatherer_double.c carma_double.c
+  $compiler $flags -o data_gatherer-mkl data_gatherer_double.c mkl_double.c
+fi
+
+echo -e "\e[01;34mWill run $ITERATIONS iterations, $REPETITIONS repetitions per iteration\e[0m"
+
+export MKL_DYNAMIC=FALSE
+
+for (( i=1; i<=$ITERATIONS; i++ )); do
+  echo -e "\e[01;36mRunning iteration $i\e[0m"
+  for alg in "${algs[@]}"; do
+    echo -e "\e[0;32mRunning $alg...\e[0m"
+    base_alg=$(cut -d "_" -f 1 <<< "$alg")
+    for (( threads=$MIN_THREADS; threads<=$MAX_THREADS; threads*=2 )); do
+      if [ $alg = "mkl" ]; then
+        export MKL_NUM_THREADS=$threads
+        carma_depth=-1
+      else
+        export MKL_NUM_THREADS=1
+        export CILK_NWORKERS=$threads
+        carma_depth=$(cut -d "_" -f 2 <<< "$alg")
+      fi
+      if [ $MODE = "sweep" ]; then
+        $run_command ./data_gatherer-$base_alg $alg $MIN_M $MIN_K $MIN_N $MAX_M $MAX_K $MAX_N $threads $carma_depth $REPETITIONS $SWEEP_PATTERN $SWEEP_CONSTANT $OUTPUT
+      elif [ $MODE = "random" ]; then
+        if [ $alg == ${algs[0]} ]; then # else use same dimensions if multiple algs
+          m=$(((($RANDOM % (($MAX_M - $MIN_M) / 32 + 1)) * 32) + $MIN_M))
+          k=$(((($RANDOM % (($MAX_K - $MIN_K) / 32 + 1)) * 32) + $MIN_K))
+          n=$(((($RANDOM % (($MAX_N - $MIN_N) / 32 + 1)) * 32) + $MIN_N))
+        fi
+        $run_command ./data_gatherer-$base_alg $alg $m $k $n $m $k $n $threads $carma_depth $REPETITIONS $SWEEP_PATTERN $SWEEP_CONSTANT $OUTPUT
+      fi
+    done
+  done
+done
+
+echo -e "\e[0;36mcollating data...\e[0m"
+if [ $MODE  = "sweep" ]; then
+  python collator.py $((ITERATIONS * REPETITIONS)) $OUTPUT
+elif [ $MODE = "random" ]; then
+  python collator.py $REPETITIONS $OUTPUT
+fi
+
+echo -e "\e[01;32mThis trial took" $SECONDS "seconds\e[0m"
+rm -rf data_gatherer-*
+
+myExit
